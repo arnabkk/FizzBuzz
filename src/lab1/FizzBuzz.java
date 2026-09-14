5ceaa9401b8bed8b230bd5e559ac990052328090
arnabkk <arnabkkarmokar@gmail.com>
2026-09-14 16:09:05 -0400

    refactored to extract helper method

--- src/lab1/FizzBuzz.java
+++ src/lab1/FizzBuzz.java
@@ -8,7 +8,7 @@ class FizzBuzz {
     public static void main(String[] args) {
         
         int i = 1;
-        while (i <= 100) {
+        while (i < 101) {
 
             doFizzBuzz(i);
             i++;
